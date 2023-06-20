3072da13581ee1f704fe2aa340938111bdbb02eb
shufaa msuko <shufaamm@gmail.com>
2023-06-20 09:11:31 +0300

    first commit

---
+++ suza/src/main/java/com/clearance/suza/SuzaApplication.java
@@ -0,0 +1,13 @@
+package com.clearance.suza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuzaApplication.class, args);
+	}
+
+}
---
+++ suza/src/test/java/com/clearance/suza/SuzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clearance.suza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
